3ae30a5992d2297e10a5431a1c5eda85837654ab
sultunlion <i.mosayyebnezhad@gmail.com>
2026-07-18 17:05:53 +0330

    fix : bug index.go with get server and client version from redis to show into home page

--- delivery/httpserver/home/index.go
+++ delivery/httpserver/home/index.go
@@ -12,12 +12,9 @@ func (h Handler) IndexPage(c *fiber.Ctx) error {
 	clientVersion, errClient := h.GithubCheckingVersionService.GetClientVersion(c.Context())
 	serverVersion, errServer := h.GithubCheckingVersionService.GetServerVersion(c.Context())
 
-	if errClient != nil {
-		return errClient
+	if errClient != nil || errServer != nil {
+		clientVersion, serverVersion = "1.0.0", "1.0.0"
 	}
 
-	if errServer != nil {
-		return errServer
-	}
 	return renderertempl.HTML(c, viewindex.Index(clientVersion, serverVersion, "https://github.com/osbn-sh/app", "https://github.com/osbn-sh/server"))
 }
